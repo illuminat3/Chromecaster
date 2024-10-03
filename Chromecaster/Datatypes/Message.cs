44cd3ef251c7d1fa3ac903822e513d4d4bbd419f
MatthewRawson <mrawson@redev.co.uk>
2024-10-03 14:18:25 +0100

    Added Message Datatype

---
+++ Chromecaster/Datatypes/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Chromecaster.Datatypes
+{
+    public class Message
+    {
+        public string Type { get; set; } = "LOAD";
+        public string Url;
+        public string ApplicationID;
+        public string NamespaceName;
+    }
+}
