26b79744cb8a83a3b649c92c6304b92408845b23
MatthewRawson <mrawson@redev.co.uk>
2024-10-03 14:18:44 +0100

    Created Locator

---
+++ Chromecaster/Services/ChromecastLocator.cs
@@ -0,0 +1,28 @@
+﻿using Chromecaster.Datatypes;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Zeroconf;
+
+namespace Chromecaster.Services
+{
+    public class ChromecastLocator
+    {
+        public async Task<List<ChromecastDevice>> DiscoverDevicesAsync()
+        {
+            List<ChromecastDevice> devices = new List<ChromecastDevice>();
+
+            IReadOnlyList<IZeroconfHost> chromecastHosts = await ZeroconfResolver.ResolveAsync("_googlecast._tcp.local.");
+
+            foreach (var host in chromecastHosts)
+            {
+                string deviceName = host.DisplayName;
+                string ipAddress = host.IPAddress;
+
+                devices.Add(new ChromecastDevice { Name = deviceName, IPAddress = ipAddress });
+            }
+
+            return devices;
+        }
+    }
+}
